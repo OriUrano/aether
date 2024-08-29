5d11e779601f5347fbebbf56aaed0a6327b187ee
Ori <77636823+u-ori@users.noreply.github.com>
2024-08-29 16:04:43 +0000

    WHY NO WORK

--- src/lib/tabWindow.svelte
+++ src/lib/tabWindow.svelte
@@ -1,91 +1,109 @@
 <script>
-    /**
+	/**
 	 * @type {number}
 	 */
-    export let size;
-    /**
+	export let size;
+	/**
 	 * @type {any}
 	 */
-     export let id;
-    export let last = false;
-    import {mouse, mouseUpOrDown} from '$lib/store'
+	export let id;
+	export let last = false;
+	import { mouse, mouseUpOrDown } from '$lib/store';
 
-    import { onMount } from 'svelte';
-    import { createEventDispatcher } from 'svelte';
+	import { onMount } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
 
-    const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-    /**
+	/**
 	 * @type {number}
 	 */
-    let mouseX;
-    /**
+	let mouseX;
+	/**
 	 * @type {number}
 	 */
-    let oldX;
-    
+	let oldX;
+
 	/**
 	 * @type {HTMLDivElement}
 	 */
 	let sideTabDragEle;
 	let sideTabDragging = false;
 
-    function sideTabDragStart() {
+	function sideTabDragStart() {
 		sideTabDragging = true;
 		document.body.style.cursor = 'col-resize';
 		sideTabDragEle.style.background = '#ec4899';
 		sideTabDragEle.style.borderRightColor = '#ec4899';
-        oldX = structuredClone(mouseX);
+		oldX = structuredClone(mouseX);
 	}
 
-    export function sideTabDragStop() {
+	export function sideTabDragStop() {
 		sideTabDragging = false;
 		document.body.style.cursor = 'default';
-        try {
-            sideTabDragEle.style.removeProperty('background');
-            sideTabDragEle.style.removeProperty('border-right-color');
-        } catch {
-            
-        }
+		try {
+			sideTabDragEle.style.removeProperty('background');
+			sideTabDragEle.style.removeProperty('border-right-color');
+		} catch {}
 	}
 
-    // @ts-ignore
-    export function sideTabDrag() {
+	// @ts-ignore
+	export function sideTabDrag() {
 		if (!sideTabDragging) {
 			return;
 		}
-		
-        document.body.style.cursor = 'col-resize';
-        sideTabDragEle.style.background = '#ec4899';
-        sideTabDragEle.style.borderRightColor = '#ec4899';
-        // size = size + (mouseX - oldX);
-        dispatch('move', {
-			move: (mouseX - oldX),
-            id: +id
+
+		document.body.style.cursor = 'col-resize';
+		sideTabDragEle.style.background = '#ec4899';
+		sideTabDragEle.style.borderRightColor = '#ec4899';
+		// size = size + (mouseX - oldX);
+		dispatch('move', {
+			move: mouseX - oldX,
+			id: +id
 		});
-        oldX = structuredClone(mouseX);
+		oldX = structuredClone(mouseX);
 	}
 
-    onMount(() => {
-        mouse.subscribe((value) => {
-            mouseX = value.x;
-            sideTabDrag();
-        });
+	onMount(() => {
+		mouse.subscribe((value) => {
+			mouseX = value.x;
+			sideTabDrag();
+		});
 
-        mouseUpOrDown.subscribe((value) => {
-            if (value) {
-                sideTabDragStop();
-            }
-        })
-    })
+		mouseUpOrDown.subscribe((value) => {
+			if (value) {
+				sideTabDragStop();
+			}
+		});
+	});
 </script>
 
 <div style="width: {size}px;}" class="h-full relative bg-neutral-900 min-w-48">
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <div class="h-12 w-full bg-neutral-700 flex pt-2 pr-6 pl-6">
-        <div class="bg-neutral-900 w-full max-w-60 h-10 pr-8 rounded-t-md relative truncate group">
-            Notes and stuff blah blah
-            <span class="transition-all absolute left-[50%] -translate-x-2/4 top-10 text-xs z-10 scale-100 z-200 bg-black rounded-md group-hover:scale-100 text-nowrap p-1 px-2 no-icon cursor-default text-white">Notes and stuff blah blah</span>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div class="h-12 w-full bg-neutral-700 flex pt-2 pr-6 pl-6 gap-3">
+		<div class="group relative">
+            <div class="truncate">
+                <div class="truncate">Notes and stuff blah blah</div>
+                <span
+                    class="transition-all absolute left-[50%] -translate-x-2/4 top-12 text-xs scale-0 z-200 bg-black rounded-md group-hover:scale-100 text-nowrap p-1 px-2 no-icon cursor-default text-white"
+                    >Notes and stuff blah blah</span
+                >
+            </div>
+		</div>
+        <div class="group relative">
+            <div class="truncate">
+                <div class="truncate">Notes and stuff blah blah</div>
+                <span
+                    class="transition-all absolute left-[50%] -translate-x-2/4 top-12 text-xs scale-0 z-200 bg-black rounded-md group-hover:scale-100 text-nowrap p-1 px-2 no-icon cursor-default text-white"
+                    >Notes and stuff blah blah</span
+                >
+            </div>
+		</div>
+		<!-- <div class="bg-neutral-700 w-full max-w-60 h-10 hover:bg-neutral-800 hover:rounded-md hover:h-8 rounded-t-md group relative whitespace-nowrap">
+            <div class="truncate pt-1 pl-4 rounded-md">
+                Notes and stuff blah blah
+            </div>
+            <span class="transition-all absolute left-[50%] -translate-x-2/4 top-12 text-xs scale-0 z-200 bg-black rounded-md group-hover:scale-100 text-nowrap p-1 px-2 no-icon cursor-default text-white">Notes and stuff blah blah</span>
             <div class="absolute h-3 w-3 -left-3 bottom-0 bg-neutral-900">
                 <div class="h-3 w-3 rounded-br-md bg-neutral-700"></div>
             </div>
@@ -93,26 +111,25 @@
                 <div class="h-3 w-3 rounded-bl-md bg-neutral-700"></div>
             </div>
         </div>
-        <div class="bg-neutral-800 w-full max-w-60 h-10 pr-8 rounded-t-md relative truncate">
-            Notes and stuff blah blah
-
-            <!-- <div class="absolute h-3 w-3 -left-3 bottom-0 bg-neutral-900">
+        <div class="bg-neutral-700 w-full max-w-60 h-10 hover:bg-neutral-800 hover:rounded-md hover:h-8 rounded-t-md group relative whitespace-nowrap">
+            <div class="truncate pt-1 pl-4 rounded-md">
+                Notes and stuff blah blah
+            </div>
+            <span class="transition-all absolute left-[50%] -translate-x-2/4 top-12 text-xs scale-0 z-200 bg-black rounded-md group-hover:scale-100 text-nowrap p-1 px-2 no-icon cursor-default text-white">Notes and stuff blah blah</span>
+            <div class="absolute h-3 w-3 -left-3 bottom-0 bg-neutral-900">
                 <div class="h-3 w-3 rounded-br-md bg-neutral-700"></div>
             </div>
             <div class="absolute h-3 w-3 -right-3 bottom-0 bg-neutral-900">
                 <div class="h-3 w-3 rounded-bl-md bg-neutral-700"></div>
-            </div> -->
-        </div>
-        <!-- <div class="bg-neutral-900 w-36 h-10">
-
+            </div>
         </div> -->
-    </div>
-    {#if !last}
-        <div
-            bind:this={sideTabDragEle}
-            class="h-screen w-[3px] bg-transparent border-r-2 border-r-neutral-700 transition-colors duration-300 hover:bg-pink-500 hover:border-r-pink-500 hover:cursor-col-resize absolute right-0 top-0"
-            on:mousedown={sideTabDragStart}
-            on:mouseup={sideTabDragStop}
-        ></div>
-    {/if}
+	</div>
+	{#if !last}
+		<div
+			bind:this={sideTabDragEle}
+			class="h-screen w-[3px] bg-transparent border-r-2 border-r-neutral-700 transition-colors duration-300 hover:bg-pink-500 hover:border-r-pink-500 hover:cursor-col-resize absolute right-0 top-0"
+			on:mousedown={sideTabDragStart}
+			on:mouseup={sideTabDragStop}
+		></div>
+	{/if}
 </div>
